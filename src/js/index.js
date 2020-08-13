88954432bd9148f9839b2fa80d0fdda12d287837
Lyyov <lyyovghidora@gmail.com>
2020-08-13 11:30:23 +0300

    add service-one page

--- src/js/index.js
+++ src/js/index.js
@@ -16,12 +16,6 @@ if(screen.width > 768) {
   $(".service-description").css("height", serviceItemHeight + "px")
   team.css("margin-top", -serviceItemHeight + "px")
   team.css("padding-top", serviceItemHeight + "px")
-  
-  if(heightSubmenu > heightSubmenu2) {
-    $(".submenu2").css("height", heightSubmenu + "px");
-  } else {
-    $(".submenu").css("height", heightSubmenu2 + "px");
-  }
 }
 
 // header && footer
@@ -94,88 +88,72 @@ $( document ).ready(function() {
   
 });
 
-// $("main").css("padding-top", headerHeight + "px");
+// images split script 
 
-// $(".slideParteners").slick({
-//   slidesToShow: 5,
-//   slidesToScroll: 1,
-//   infinite: true,
-//   autoplay: false,
-//   autoplaySpeed: 2000,
-//   centerMode: true,
-//   arrows: true,
-//   dots: false,
-//   variableWidth: true,
-//   speed: 500
-// });
-
-// $(slider).slick({
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   infinite: true,
-//   autoplay: false,
-//   autoplaySpeed: 5000,
-//   centerMode: true,
-//   arrows: true,
-//   dots: false,
-//   speed: 500,
-//   draggable: false,
-//   responsive: [
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         slidesToShow: 1
-//       }
-//     }
-//   ]
-// });
-
-// $(".sliderHome").slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   infinite: true,
-//   autoplay: true,
-//   autoplaySpeed: 3000,
-//   centerMode: false,
-//   arrows: true,
-//   dots: false,
-//   speed: 500,
-//   pauseOnHover: false
-// });
-
-// $(sliderItem).click(function(e) {
-//   target1 = $(e.target).parents(".slick-slide").attr("data-slick-index")
-//   $('#zoomModal').modal({
-//     show: true
-//   });
-// });
-
-
-
-$('#zoomModal').on('show.bs.modal', function (e) {
-  $(".zoomSlider").slick({
-    initialSlide: parseInt(target1),
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    centerMode: true,
-    arrows: true,
-    dots: false,
-    speed: 500
-  });
-})
-
-$('#zoomModal').on('hidden.bs.modal', function (e) {
-  $(".zoomSlider").slick("unslick")
-});
-
-$(".modalContainer").click(function(e) {
-  if($(this) === $(e.target)) {
-    $('#zoomModal').modal('hide');
-  }
+// $( document ).ready(function() {
+//   const dragItem= document.getElementById("image-split");
+//   const dragPointer= document.getElementById("drag");
+//   const imageClean = document.getElementById("split-container-clean");
   
-})
+//   let active = false,
+//       elWidth = dragItem.offsetWidth,
+//       currentX,
+//       initialX,
+//       xOffset = 0;
 
-    
+//       function dragStart(e) {
+//         e.preventDefault();
+//         if (e.type === "touchstart") {
+          
+//           initialX = e.touches[0].clientX - xOffset;
+//         } else {
+//           initialX = e.clientX - xOffset;
+//         }
+  
+//         if (e.target === dragPointer) {
+//           active = true;
+//         }
+//       }
+
+//       function dragEnd(e) {
+//         initialX = currentX;
+        
+//         active = false;
+//       }
+
+//       function drag(e) {
+//         if (active) {
+//           e.preventDefault();
+          
+//           if (e.type === "touchmove" && e.target === dragPointer) {
+//             console.log(e.touches[0].clientX - initialX)
+//             currentX = e.touches[0].clientX - initialX;
+//           } else {
+//             currentX = e.clientX - initialX;
+//           }
+  
+//           xOffset = currentX;
+//           console.log(currentX)
+  
+//           setClip(currentX, dragPointer);
+//         }
+//       }
+  
+//       function setClip(xPos, el) {
+//         if (xPos > 0 && xPos < elWidth - 10) {
+//           el.style.left = xPos + "px";
+//         }
+//       }
+
+//       dragItem.addEventListener("touchstart", dragStart, false);
+//       dragItem.addEventListener("touchend", dragEnd, false);
+//       dragItem.addEventListener("touchmove", drag, false);
+  
+//       dragItem.addEventListener("mousedown", dragStart, false);
+//       dragItem.addEventListener("mouseup", dragEnd, false);
+//       dragItem.addEventListener("mousemove", drag, false);
+
+//       document.addEventListener("mouseup", () => {
+//         active = false
+//       })
+// });
